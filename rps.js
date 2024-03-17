323b51a2a49d4fb914d017e40d1dfe7839b472c6
Louie <160453057+Louie3588@users.noreply.github.com>
2024-03-17 13:31:35 -0500

    Adjust the letter case of the first if statement
    Functions are case sensitive and an Uppercase "If"
    statement will not be recognized. Adjustment to "if"
    will allow for the if statement to be run.

--- rps.js
+++ rps.js
@@ -3,9 +3,18 @@ function getComputerChoice(){
 let computerChoice;
 //assign the variable for the computer choice a random number of 1-3
 computerChoice = Math.floor(Math.random() * 3) + 1;
+console.log(computerChoice);
 //assign the number 1 to represent rock
-
+if (computerChoice === 1){
+ computerChoice = "Rock";
 //assign the number 2 to represent paper
+} else if (computerChoice === 2) {
+    computerChoice = "Paper";
+    
 //assign the number 3 to represent scissors
+} else {
+    computerChoice = "Scissors";
+}
 //then output the result 
+return computerChoice;
 }
