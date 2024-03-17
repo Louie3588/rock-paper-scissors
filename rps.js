f18c062f5ea45bbe5a1fcd39664465ea6a6af886
Louie <160453057+Louie3588@users.noreply.github.com>
2024-03-17 12:19:49 -0500

    Create the getComputer function
    the getComputer function will randomly generate
    a number between 1 & 3 including 1 and 3. Then it
    will change the number into a string of Rock, Paper
    or Scissors. Then the function will return the result

---
+++ rps.js
@@ -0,0 +1,11 @@
+function getComputerChoice(){
+//create a variable for the computer choice
+let computerChoice;
+//assign the variable for the computer choice a random number of 1-3
+computerChoice = Math.floor(Math.random() * 3) + 1;
+//assign the number 1 to represent rock
+
+//assign the number 2 to represent paper
+//assign the number 3 to represent scissors
+//then output the result 
+}
